40801122e23dd248b530e92cb05a4bd1edae5674
Eniola Hannah <heniolahannah@gmail.com>
2025-01-20 15:51:54 +0100

    Creates Products Table

--- Query.sql
+++ Query.sql
@@ -9,3 +9,11 @@ Email VARCHAR(50) NOT NULL,
 Phone_Number VARCHAR(20),
 Address VARCHAR(100));
 DESCRIBE Customers;
+
+CREATE TABLE Products(
+Product_id INT PRIMARY KEY,
+Product_name VARCHAR(50) NOT NULL,
+Category VARCHAR(50),
+Price INT NOT NULL,
+Stock_quantity INT);
+DESCRIBE Products;
