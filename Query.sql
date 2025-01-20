99681b1296e422bfbd131f3c21c342d7759a7fa5
Eniola Hannah <heniolahannah@gmail.com>
2025-01-20 15:45:25 +0100

    Creates Customer with its data types and contsraints

--- Query.sql
+++ Query.sql
@@ -1,1 +1,12 @@
-CREATE DATABASE E_COMMERCE;
+CREATE DATABASE E_commerce;
+
+USE E_commerce;
+
+CREATE TABLE Customers(
+Customer_id INT PRIMARY KEY,
+Name VARCHAR(100) NOT NULL,
+Email VARCHAR(50) NOT NULL,
+Phone_Number VARCHAR(20),
+Address VARCHAR(100));
+
+DESCRIBE Customers;
