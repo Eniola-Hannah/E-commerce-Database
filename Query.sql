1bb6ac61266f952730ade03c482cbc7d6dafa4e5
Eniola Hannah <heniolahannah@gmail.com>
2025-01-20 15:48:56 +0100

    Adds a new constraint at Customer Table

--- Query.sql
+++ Query.sql
@@ -3,10 +3,9 @@ CREATE DATABASE E_commerce;
 USE E_commerce;
 
 CREATE TABLE Customers(
-Customer_id INT PRIMARY KEY,
+Customer_id INT AUTO_INCREMENT PRIMARY KEY,
 Name VARCHAR(100) NOT NULL,
 Email VARCHAR(50) NOT NULL,
 Phone_Number VARCHAR(20),
 Address VARCHAR(100));
-
 DESCRIBE Customers;
